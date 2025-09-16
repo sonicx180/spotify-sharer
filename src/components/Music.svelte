a7fb148c973049cf4470c6ebe824fa9c3b9069a0
Sonicx180 <83236320+sonicx180@users.noreply.github.com>
2025-09-15 21:21:12 -0700

    started to work on music component

--- src/components/Music.svelte
+++ src/components/Music.svelte
@@ -1,9 +1,18 @@
 <script>
 
+let { img_src = '../music_note.png'} = $props();
 </script>
 
 <div class = "music-box">
-    <img src = "../music_note.png" alt = "A music note" />
-    <h3> Name of Music Artist</h3>
+    <img src = {img_src} alt = "A music note" />
+    <h3> Name of Music</h3> by <h4> Music Artist</h4>
     <h3> User's Name</h3>
 </div>
+
+<style>
+ .music-box {
+    background-color: var(--bg-lighter);
+    padding:10px;
+    border-radius:5px;
+ }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,12 @@
 </script>
 
 <main class="main-page">
+    <h1> Spotify Sharer</h1>
 	<h2>See what people are listening to at the moment.</h2>
 
 	<div class="music-wrapper">
+		<Music />
+		<Music />
 		<Music />
 	</div>
 </main>
@@ -14,4 +17,10 @@
     .main-page {
         text-align:center;
     }
+	.music-wrapper {
+		display:flex;
+		justify-content: space-between;
+		margin:10px;
+	}
+
 </style>
